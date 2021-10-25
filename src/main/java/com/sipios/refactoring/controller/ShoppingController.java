80a2b9e9f03f5b527b0a1497f388e672073736c0
Corentin.Vives <corentin.vives@insa-rennes.fr>
2021-10-25 18:17:12 +0200

    [fix] ShoppingController: useless import removed

--- src/main/java/com/sipios/refactoring/controller/ShoppingController.java
+++ src/main/java/com/sipios/refactoring/controller/ShoppingController.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
-import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
